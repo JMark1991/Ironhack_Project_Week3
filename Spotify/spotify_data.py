f87968fd497617d5d08ca86991e077eb5f8d1830
Melissa Badrudin <melissabadrudin93@gmail.com>
2020-07-15 16:50:04 +0100

    commit
    created and treated spotify_df

---
+++ Spotify/spotify_data.py
@@ -0,0 +1,32 @@
+
+
+
+# Loading and cleaning the spotify data
+
+import pandas as pd
+
+
+#create DataFrame and print head
+
+spotify_df= pd.read_csv("Spotify/data.csv")
+print(spotify_df.head())
+
+#Transform the dtype of the column year to numeric so I can apply condition in sliccing
+
+spotify_df["year"] = pd.to_numeric(spotify_df["year"])
+
+#Create condition
+condition = spotify_df["year"] >= 1960
+
+#Apply Condition to dataframe
+spotify_df = spotify_df[condition]
+
+#print head to confirm if condition was applied
+print(spotify_df["year"].head())
+
+#change dtype of column "year" from float to int
+spotify_df["year"] = spotify_df["year"].astype(int)
+
+#confirm the column is in dtype int64
+print(spotify_df["year"].head())
+
