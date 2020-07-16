038354b74316a3f0670d838151a0d52de734de39
JMark1991 <jormarq1@hotmail.com>
2020-07-16 01:23:46 +0100

    A few comments on the code

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ billboard_df = pd.read_csv('Billboard/billboards.csv')
 billboard_df.dropna(inplace=True)       #print(billboard_df.isnull().sum())
 billboard_df['Top'] = billboard_df['Top'].astype(int)       #print(billboard_df.dtypes)
 
+# create a table with song_name and artist_name, drop_duplicates, create a temporary_id for each song
 music_artist_df = billboard_df[['Song_Name','Artist_Name']].drop_duplicates()
 music_artist_df['temp_song_ID'] = range(0,len(music_artist_df))
 
+# add the temp_song_ID to the billboard_df
 billboard_df = billboard_df.merge(music_artist_df,how='left',on=['Song_Name','Artist_Name'])
 
+# create a Popularity_score based on the positions on the top
 billboard_df['Popularity_Score'] = 101-billboard_df['Top']
 
+# create a 'Year' column
 billboard_df['Year'] = billboard_df['Date'].apply(lambda dt : dt[0:4])
 
+# group by year and song to get the popularity scores per year
 yearly_scores = billboard_df.groupby(['Year','temp_song_ID','Song_Name','Artist_Name'])['Popularity_Score'].sum()
-#
+
 
 
 print(yearly_scores)
