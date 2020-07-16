3215b7114d353c326d9c85ecc1e87dbb6172dd33
JMark1991 <jormarq1@hotmail.com>
2020-07-16 17:31:58 +0100

    Completed the music_artist_df function

--- main.py
+++ main.py
@@ -3,18 +3,61 @@ import pandas as pd
 import re
 
 
-#clean spotify database
+
 def clean_spotify_data(): 
+    # read the spotify_database
+    # clean the data
+    # output: clean_database
     spotify_df= pd.read_csv("Spotify/data.csv")
     spotify_df["year"] = pd.to_numeric(spotify_df["year"])
     condition = spotify_df["year"] >= 1960
     spotify_df = spotify_df[condition]
     spotify_df["year"] = spotify_df["year"].astype(int)
+    spotify_df.drop_duplicates(['name','artists'],inplace=True)
     return spotify_df
 
-spotify_df=clean_spotify_data()
 
-# merging the 2 data sources and doing the analysis
+def find_spotify_id(music_artist_df,spotify_df):
+    # input: music_artist_df, spotify_df
+    # Find the spotify_id of the songs
+    # output: updated music_artist_df
+
+
+    # split the artists by: "&"" and "feat"
+    rules = '\s*&\s*|\s+[Ff]eat\w*\s+'
+    music_artist_df['Primary_Artist'] = music_artist_df['Artist_Name'].apply(lambda art : re.split(rules,art)[0])
+    music_artist_df['Artists_List'] = music_artist_df['Artist_Name'].str.split(rules)
+
+
+    # Match song names not duplicated
+    remove_song_dup_df = music_artist_df.drop_duplicates(['Song_Name'])
+    remove_song_dup_df = remove_song_dup_df.merge(spotify_df[['name','id']], how='inner', left_on='Song_Name', right_on='name')
+    music_artist_df = music_artist_df.merge(remove_song_dup_df[['temp_song_ID','id']], how='left', on= 'temp_song_ID')
+
+
+    # Match the song and the first artist
+    temp_spot_df = spotify_df[['name', 'artists','id']]
+    temp_spot_df['primary_artist'] = temp_spot_df['artists'].apply(lambda art : re.split("\'\,",art)[0].replace("['","").replace("']",""))
+
+    temp_music_artist_df = music_artist_df[music_artist_df['id'].isnull()]
+    temp_music_artist_df.drop('id', axis=1, inplace=True)
+    temp_music_artist_df = temp_music_artist_df.merge(temp_spot_df, how='inner', left_on=['Song_Name','Primary_Artist'], right_on=['name','primary_artist'])
+
+    music_artist_df = music_artist_df.append(temp_music_artist_df[['Song_Name', 'Artist_Name', 'temp_song_ID', 'Primary_Artist','Artists_List', 'id']])
+    music_artist_df.drop_duplicates('temp_song_ID',keep='last',inplace=True)
+    
+    # Remove the remaining spotify_id NaNs 
+    music_artist_df = music_artist_df[music_artist_df['id'].notna()]
+    
+    #print('music_artist_df: \n',music_artist_df)
+    
+    return music_artist_df
+
+
+
+# read and clean spotify data
+spotify_df = clean_spotify_data()
+
 
 # billboard: read, remove nulls, drop duplication, convert data types
 billboard_df = pd.read_csv('Billboard/billboards.csv')
@@ -22,12 +65,24 @@ billboard_df.dropna(inplace=True)       #print(billboard_df.isnull().sum())
 billboard_df.drop_duplicates(['Top','Song_Name','Artist_Name','Date'],inplace=True)
 billboard_df['Top'] = billboard_df['Top'].astype(int)       #print(billboard_df.dtypes)
 
+
 # create a table with song_name and artist_name, drop_duplicates, create a temporary_id for each song
 music_artist_df = billboard_df[['Song_Name','Artist_Name']].drop_duplicates()
 music_artist_df['temp_song_ID'] = range(0,len(music_artist_df))
 
+
 # add the temp_song_ID to the billboard_df
 billboard_df = billboard_df.merge(music_artist_df,how='left',on=['Song_Name','Artist_Name'])
+
+# Update the music_artist_df with the spotify song ids
+music_artist_df = find_spotify_id(music_artist_df,spotify_df)
+
+# Merge dataframes
+####### Work in progress
+print(music_artist_df)
+
+
+# Data Analysis
 
 # create a Popularity_score based on the positions on the top
 billboard_df['Popularity_Score'] = 101-billboard_df['Top']
@@ -37,39 +92,18 @@ billboard_df['Year'] = billboard_df['Date'].apply(lambda dt : dt[0:4])
 
 # group by year and song to get the popularity scores per year
 yearly_scores = billboard_df.groupby(['Year','temp_song_ID','Song_Name','Artist_Name'])['Popularity_Score'].sum()
+# agr
 
-# split the artists by: & , feat 
-rules = '\s*&\s*|\s+[Ff]eat.*\s+'
-music_artist_df['Artist_List'] = music_artist_df['Artist_Name'].str.split(rules)
+
 
 
 # try to match song names and song_IDs
 
 
-def find_spotify_id(music_artist_df,spotify_df):
-    # Match song names not duplicated
-    remove_song_dup_df = music_artist_df.drop_duplicates(['Song_Name'])
-    remove_song_dup_df = remove_song_dup_df.merge(spotify_df[['name','id']], how='inner', left_on='Song_Name', right_on='name')
-    music_artist_df = music_artist_df.merge(remove_song_dup_df[['temp_song_ID','id']], how='left', on= 'temp_song_ID')
-
-    # Match by Song and artist names
-
-
-    # Match the song and the first artist
 
 
 
-    # Check the remaining spotify_id NaNs 
 
-
-
-    print('music_artist_df correct ids: ',len(music_artist_df) - music_artist_df['id'].isnull().sum())
-    print('music_artist_df nulls: ',music_artist_df['id'].isnull().sum())
-
-    
-    
-    
-find_spotify_id(music_artist_df,spotify_df)
 
 
 
